b6d630c9c0a04e6d1ca19272159723402d48e29e
DongKon Lee <dle0129@gmail.com>
2021-01-20 09:11:54 +0900

    install Storybook

---
+++ App/stories/test.stories.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import {storiesOf} from '@storybook/react-native';
+import Test from '../components/Test'
+
+storiesOf("Test", module)
+.add('default', () => (
+    <Test/>
+))
---
+++ storybook/storyLoader.js
@@ -0,0 +1,12 @@
+function loadStories() {
+  require('../App/stories/Test.stories');
+}
+
+const stories = [
+  '../App/stories/Test.stories'
+];
+
+module.exports = {
+  loadStories,
+  stories,
+};
